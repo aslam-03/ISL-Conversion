4b2e0c81bc1fd61fec27f0b47da4593de2600920
Mohamed Aslam I <152874124+aslam-03@users.noreply.github.com>
2025-02-07 13:10:41 +0530

    Update process.py

--- process.py
+++ process.py
@@ -1,37 +1,37 @@
 import os
-from tensorflow.keras.preprocessing.image import ImageDataGenerator # type: ignore
+from tensorflow.keras.preprocessing.image import ImageDataGenerator 
 
 # Define dataset path
 dataset_path = 'C:\\Users\\ASLAM\\Desktop\\ISL\\dataset\\original_images'
 
 # Create ImageDataGenerator for data augmentation and rescaling
 datagen = ImageDataGenerator(
-    rescale=1./255,          # Normalize pixel values
-    rotation_range=30,       # Randomly rotate images by 30 degrees
-    width_shift_range=0.2,   # Randomly shift images horizontally
-    height_shift_range=0.2,  # Randomly shift images vertically
-    shear_range=0.2,         # Apply shearing transformation
-    zoom_range=0.2,          # Randomly zoom images
-    horizontal_flip=True,    # Randomly flip images horizontally
-    fill_mode='nearest',     # Fill missing pixels after transformations
-    validation_split=0.2     # Reserve 20% of the data for validation
+    rescale=1./255,          
+    rotation_range=30,      
+    width_shift_range=0.2,   
+    height_shift_range=0.2,  
+    shear_range=0.2,         
+    zoom_range=0.2,          
+    horizontal_flip=True,    
+    fill_mode='nearest',     
+    validation_split=0.2     
 )
 
 # Create generators for training and validation data
 train_generator = datagen.flow_from_directory(
     dataset_path,
-    target_size=(250, 250),  # Resize images to 250x250
-    batch_size=32,           # Set the batch size
-    class_mode='categorical', # Use categorical labels
-    subset='training'        # Use training subset
+    target_size=(250, 250),  
+    batch_size=32,           
+    class_mode='categorical', 
+    subset='training'        
 )
 
 validation_generator = datagen.flow_from_directory(
     dataset_path,
-    target_size=(250, 250),  # Resize images to 250x250
-    batch_size=32,           # Set the batch size
-    class_mode='categorical', # Use categorical labels
-    subset='validation'      # Use validation subset
+    target_size=(250, 250),  
+    batch_size=32,           
+    class_mode='categorical', 
+    subset='validation'      
 )
 
 # Get number of classes
